bf4e2fac64a2a8896796c588a5e3c5cb7f4aabe6
Senura Dissanayake <delgahadeniya.dissanayake@students.plymouth.ac.uk>
2018-02-18 22:33:58 +0530

    added the button click events for tghe MainMenu

--- app/src/main/java/com/example/senura/healthxstream/FamilyMembersActivity.java
+++ app/src/main/java/com/example/senura/healthxstream/FamilyMembersActivity.java
@@ -2,6 +2,12 @@ package com.example.senura.healthxstream;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FamilyMembersActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class FamilyMembersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_members);
+
+
+
+        // Get reference of widgets from XML layout
+        final ListView lv = (ListView) findViewById(R.id.ListView_FamilyMems);
+
+        // Initializing a new String Array
+        String[] fruits = new String[] {
+                "Malaka - Me",
+                "Monalee - Sister",
+                "Damayanthi - Mom",
+                "Anura - Dad"
+
+        };
+
+        // Create a List from String Array elements
+        final List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
+
+        // Create an ArrayAdapter from List
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, fruits_list);
+
+        // DataBind ListView with items from ArrayAdapter
+        lv.setAdapter(arrayAdapter);
+
     }
 }
--- app/src/main/java/com/example/senura/healthxstream/MainActivity.java
+++ app/src/main/java/com/example/senura/healthxstream/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.senura.healthxstream;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,75 @@ public class MainActivity extends AppCompatActivity {
         //email added delgahadeniya.dissanayake@students.plymouth.ac.uk
 
 
-        Intent myIntent = new Intent(MainActivity.this, MyDoctorsActivity.class);
-        myIntent.putExtra("key", "Achala Dissanayake"); //Optional parameters
-        MainActivity.this.startActivity(myIntent);
+       setButtons();
 
+
+
+
+
+    }
+
+
+
+
+    private void setButtons(){
+        ImageButton button_HB;
+        ImageButton button_FM;
+        ImageButton button_MD;
+        ImageButton button_BT;
+
+
+
+        //BodyTemp
+        button_MD = (ImageButton) findViewById(R.id.imageButton_MM_BT);
+
+        button_MD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BodyTemperatureActivity.class));
+            }
+        });
+
+
+        //MyDocs
+        button_MD = (ImageButton) findViewById(R.id.imageButton_MM_MD);
+
+        button_MD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(MainActivity.this, MyDoctorsActivity.class);
+                myIntent.putExtra("key", "Achala Dissanayake"); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+
+            }
+        });
+
+
+
+
+        //FamilyMems
+        button_FM = (ImageButton) findViewById(R.id.imageButton_MM_FM);
+
+        button_FM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FamilyMembersActivity.class));
+
+            }
+        });
+
+
+        //hearbeat
+        button_HB = (ImageButton) findViewById(R.id.imageButton_MM_HB);
+
+        button_HB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HeartBeatActivity.class));
+
+            }
+        });
     }
 
 
