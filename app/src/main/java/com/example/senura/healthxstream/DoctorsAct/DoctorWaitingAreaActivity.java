3740b8951b746dd3c2ec7996a73bf2fb53447962
SenuraD <senurad@99x.lk>
2018-05-03 14:57:53 +0530

    added the MQTT controllers to the DocWaiting act

--- app/src/main/java/com/example/senura/healthxstream/DoctorsAct/DoctorWaitingAreaActivity.java
+++ app/src/main/java/com/example/senura/healthxstream/DoctorsAct/DoctorWaitingAreaActivity.java
@@ -3,22 +3,46 @@ package com.example.senura.healthxstream.DoctorsAct;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.senura.healthxstream.BodyTemperatureActivity;
 import com.example.senura.healthxstream.MainActivity;
+import com.example.senura.healthxstream.MyDoctorsActivity;
 import com.example.senura.healthxstream.R;
+import com.example.senura.healthxstream.mqttConnectionPackage.MqttConnection;
+import com.example.senura.healthxstream.mqttConnectionPackage.uniqueIDgenerator;
 
-public class DoctorWaitingAreaActivity extends AppCompatActivity {
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DoctorWaitingAreaActivity extends AppCompatActivity implements MqttCallback {
+
+    //MQTT Vars
+    private MqttConnection mConnection = new MqttConnection();
+    private MqttAndroidClient client = null;
+    private boolean isPublished = false;
+    public String SourceID;
+    public String jsonResponse = null;
+    String doctorID=null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_waiting_area);
 
+        doctorID="345";//should get the docID from the login
         setButtons();
 
     }
@@ -43,6 +67,46 @@ public class DoctorWaitingAreaActivity extends AppCompatActivity {
                 hideSearchTextArea();
             }
         });
+
+    }
+
+
+    private void connectMqttClient() {
+        client = mConnection.connect(DoctorWaitingAreaActivity.this, this, "healthxtream/doctor/"+doctorID, false);
+        client.setCallback(DoctorWaitingAreaActivity.this);
+    }
+
+
+    public boolean passPayload(String payload) {
+
+        String passingPayload = payload;
+
+        String passingTopic = "healthxtream/send/";
+        isPublished=false;
+
+        isPublished = mConnection.publishMessage(passingPayload, passingTopic);
+
+        if (isPublished)
+            Toast.makeText(DoctorWaitingAreaActivity.this,"Message Published", Toast.LENGTH_SHORT).show();
+
+        Log.d("TagpassedPayload", passingPayload);
+
+        return isPublished;
+    }
+
+
+    @Override
+    public void connectionLost(Throwable cause) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
 
     }
 
