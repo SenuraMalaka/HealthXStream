df029c7aa90bb7d562e38519d041204e46c331ee
Senura Dissanayake <delgahadeniya.dissanayake@students.plymouth.ac.uk>
2018-05-07 17:47:29 +0530

    Added docNeedIllnessAct

---
+++ app/src/main/java/com/example/senura/healthxstream/DoctorsAct/DocNeedIllnessActivity.java
@@ -0,0 +1,19 @@
+package com.example.senura.healthxstream.DoctorsAct;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.senura.healthxstream.R;
+
+public class DocNeedIllnessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_need_illness);
+    }
+
+    
+
+
+}
